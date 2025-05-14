d63d100f1045d675d0fbb4dae0f7a1d940cdef8f
inekruz <nekruz15ik@gmail.com>
2025-05-14 15:49:24 +0500

    added nonce with unique token

--- client/src/components/SecureToken.js
+++ client/src/components/SecureToken.js
@@ -2,9 +2,10 @@ import SHA256 from 'crypto-js/sha256';
 
 export function sendSecureRequest(userId) {
   const timestamp = Date.now();
-  const raw = `${userId}:${process.env.REACT_APP_KEY}`;
+  const nonce = Math.random().toString(36).substring(2, 10);
+  const raw = `${userId}:${process.env.REACT_APP_KEY}:${nonce}`;
   const token = SHA256(raw).toString();
 
-  const tokenWithTimestamp = `${timestamp}-${token}`;
+  const tokenWithTimestamp = `${timestamp}-${token}-${nonce}`;
   return tokenWithTimestamp;
 }
--- server/api/routes/middleware/validateAuthToken.js
+++ server/api/routes/middleware/validateAuthToken.js
@@ -3,8 +3,8 @@ const db = require('../../../db');
 
 const secretKey = process.env.AUTH_TOKEN;
 
-const generateToken = (userId) => {
-  const data = `${userId}:${secretKey}`;
+const generateToken = (userId, nonce) => {
+  const data = `${userId}:${secretKey}:${nonce}`;
   return crypto.createHash('sha256').update(data).digest('hex');
 };
 
@@ -16,8 +16,8 @@ const validateAuthToken = async (req, res, next) => {
     return res.status(400).json({ message: 'Ты ничего не забыл добавить :)' });
   }
 
-  const [timestampStr, token] = clientToken.split('-');
-  if (!timestampStr || !token) {
+  const [timestampStr, token, nonce] = clientToken.split('-');
+  if (!timestampStr || !token || !nonce) {
     return res.status(400).json({ message: 'Неверный формат токена' });
   }
 
@@ -26,28 +26,27 @@ const validateAuthToken = async (req, res, next) => {
     return res.status(400).json({ message: 'Неверный timestamp' });
   }
 
-  // Проверка возраста токена
   const tokenAgeHours = (Date.now() - timestamp) / (1000 * 60 * 60);
   if (tokenAgeHours > 24) {
     return res.status(403).json({ message: 'Фу, токен слишком старый' });
   }
 
-  const expectedToken = generateToken(user_id);
-
+  const expectedToken = generateToken(user_id, nonce);
   if (token !== expectedToken) {
     return res.status(403).json({ message: 'Блин, а токен то неверный :)' });
   }
 
   try {
-    // Проверка, был ли уже использован
     const result = await db.query('SELECT * FROM wh_used_tokens WHERE token = $1', [clientToken]);
 
     if (result.rows.length > 0) {
       return res.status(403).json({ message: 'Такой токен уже использовали! Не жульничай!' });
     }
 
-    // Добавление токена
-    await db.query('INSERT INTO wh_used_tokens (token, user_id, created_at) VALUES ($1, $2, NOW())', [clientToken, user_id]);
+    await db.query(
+      'INSERT INTO wh_used_tokens (token, user_id, created_at) VALUES ($1, $2, NOW())',
+      [clientToken, user_id]
+    );
   } catch (error) {
     console.error('Ошибка при проверке токена:', error);
     return res.status(500).json({ message: 'Внутренняя ошибка сервера' });
